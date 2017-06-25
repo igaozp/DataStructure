7a9d4ebbc4a3ecc302976eb6da5cd5bfacf0b109
igaozp <igaozp@hotmail.com>
2017-06-25 11:19:01 +0800

    Fix Stack.py

--- Stack.py
+++ Stack.py
@@ -1,24 +1,24 @@
 class Stack:
-    def __init__(self, datas=[]):
+    def __init__(self, data=[]):
         # 使用List保存数据
-        self.datas = datas
+        self.data = data
 
     def __len__(self):
         # 计算栈内的数据个数
-        return len(self.datas)
+        return len(self.data)
 
-    def isEmpty(self):
+    def is_empty(self):
         # 检查栈是否为空
         return len(self) == 0
 
     def push(self, data):
         # 压栈，添加一个元素
-        self.datas.append(data)
+        self.data.append(data)
 
     def pop(self):
         # 栈顶元素出栈，如果栈为空则返回None
-        return self.datas.pop() if not self.isEmpty() else None
+        return self.data.pop() if not self.is_empty() else None
 
     def peek(self):
         # 查看栈顶元素
-        return self[len(self.datas) - 1] if not self.isEmpty() else None
+        return self[len(self.data) - 1] if not self.is_empty() else None
